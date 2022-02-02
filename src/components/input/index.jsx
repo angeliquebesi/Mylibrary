7018ed3ca7e4b4f4946004ba9120363c9c0c1808
angeliquebesi <besineaud85@gmail.com>
2022-02-02 18:46:02 +0100

    mise en forme form

--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -1,4 +1,5 @@
 import InputText from './InputText';
 import InputTextArea from './InputTextArea';
+import InputNumber from './InputNumber';
 
-export { InputText, InputTextArea };
+export { InputText, InputTextArea, InputNumber };
--- src/container/AddBook.jsx
+++ src/container/AddBook.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import { InputText, InputTextArea } from '../components/input';
+import { InputNumber, InputText, InputTextArea } from '../components/input';
 import Title from '../components/title/Title';
 
 const AddBook = function AddBook() {
   return (
     <div>
       <Title text="Add a book" />
-      <div>
-        <InputText
-          titreLabel="Title"
-          defaultValue="ex : Harry Potter and the
+      <div className='"flex justify-center items-center h-screen w-full'>
+        <div className="mx-2 md:mx-auto md:w-1/2 bg-white rounded shadow-2xl p-8 m-4">
+          <InputText
+            titreLabel="Title"
+            defaultValue="ex : Harry Potter and the
 Philosopher's Stone"
-        />
-        <InputText titreLabel="Author" defaultValue="ex : J.K. Rowling " />
-        <InputText titreLabel="Category" defaultValue="ex : Fantasy" />
-        <InputTextArea titreLabel="Summary" />
+          />
+          <InputText titreLabel="Author" defaultValue="ex : J.K. Rowling " />
+          <InputText titreLabel="Category" defaultValue="ex : Fantasy" />
+          <InputNumber titreLabel="Year" />
+          <InputText titreLabel="Image" defaultValue="link http:// " />
+          <InputTextArea titreLabel="Summary" />
+        </div>
       </div>
     </div>
   );
