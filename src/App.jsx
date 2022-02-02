1bf3af9562f4aff4b80618d9d104f0b70de3a152
angeliquebesi <besineaud85@gmail.com>
2022-02-02 11:44:40 +0100

    réalisation de la route home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from './container';
 
-function App() {
+const App = function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
