c76a8d283090604751dd87d0ed32d6aba25e9731
angeliquebesi <besineaud85@gmail.com>
2022-02-02 11:28:44 +0100

    mise en place du component Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import NavBar from './components/navbar/NavBar';
 import Home from './container';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Home />
     </div>
   );
