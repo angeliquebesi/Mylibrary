7b2927c92cae04d9381b6930850acd4787743ba8
angeliquebesi <besineaud85@gmail.com>
2022-02-02 15:10:12 +0100

    préparation container allbooks et components title

--- src/container/AllBooks.jsx
+++ src/container/AllBooks.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import Title from '../components/title/Title';
 
 const AllBooks = function AllBooks() {
-  return <div>AllBooks</div>;
+  return (
+    <div>
+      <Title />
+    </div>
+  );
 };
 export default AllBooks;
