635043015a0c50b1cab60c8eff4b47c4a3c8c8be
angeliquebesi <besineaud85@gmail.com>
2022-02-02 20:17:27 +0100

    fix prb merge

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 
 const Title = function Title({ text }) {
   return (
-    <div>
-      <h2 className="text-center text-5xl font-normal leading-normal mt-0 mb-2 text-indigo-800">
-        {text}
-      </h2>
+    <div className="text-center text-5xl font-normal leading-normal mt-0 mb-2 text-indigo-800">
+      <h2> {text} </h2>
     </div>
   );
 };
+
 export default Title;
