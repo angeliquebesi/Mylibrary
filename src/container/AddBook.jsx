b6c3334568033f01cca9f074a022081bc99875e0
angeliquebesi <besineaud85@gmail.com>
2022-02-02 17:45:57 +0100

    mise en place du formulaire pour ajouter un livre

--- src/container/AddBook.jsx
+++ src/container/AddBook.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { InputText, InputTextArea } from '../components/input';
+import Title from '../components/title/Title';
 
 const AddBook = function AddBook() {
-  return <div>AddBook</div>;
+  return (
+    <div>
+      <Title text="Add a book" />
+      <div>
+        <InputText
+          titreLabel="Title"
+          defaultValue="ex : Harry Potter and the
+Philosopher's Stone"
+        />
+        <InputText titreLabel="Author" defaultValue="ex : J.K. Rowling " />
+        <InputText titreLabel="Category" defaultValue="ex : Fantasy" />
+        <InputTextArea titreLabel="Summary" />
+      </div>
+    </div>
+  );
 };
 export default AddBook;
